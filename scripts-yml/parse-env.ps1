4ea613348c89b6aecf9258eafa72f5bb725ead74
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-09-06 11:09:12 +0700

    CODING

--- scripts-yml/parse-env.ps1
+++ scripts-yml/parse-env.ps1
@@ -2,6 +2,7 @@
 
 # Get the inputs JSON from environment variable
 $inputsJson = $env:toJSON_inputs
+$secretsJson = $env:toJSON_secrets
 
 # Check if the inputsJson is not null or empty
 if (-not [string]::IsNullOrEmpty($inputsJson)) {
@@ -17,4 +18,20 @@ if (-not [string]::IsNullOrEmpty($inputsJson)) {
 }
 else {
     Write-Host "No inputs found or inputs JSON is null."
+}
+
+# Check if the secretsJson is not null or empty
+if (-not [string]::IsNullOrEmpty($secretsJson)) {
+    # Parse the JSON
+    $inputs = $secretsJson | ConvertFrom-Json
+
+    # Now you can access each input like:
+    foreach ($key in $inputs.PSObject.Properties.Name) {
+        $envVarName = "o_secrets_$($key)"
+        $value = $inputs.$key
+        echo "$envVarName=$value"
+    }
+}
+else {
+    Write-Host "No secret found or secrets JSON is null."
 }
