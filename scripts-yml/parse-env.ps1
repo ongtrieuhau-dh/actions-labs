87328592dbc997bbbdb2dcf61b311c3d73d966c9
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-09-06 11:05:41 +0700

    CODING

--- scripts-yml/parse-env.ps1
+++ scripts-yml/parse-env.ps1
@@ -1,14 +1,20 @@
 # parse-env.ps1
 
 # Get the inputs JSON from environment variable
-$inputsJson = $env:INPUTS_JSON
+$inputsJson = $env:toJSON_inputs
 
-# Parse the JSON
-$inputs = $inputsJson | ConvertFrom-Json
+# Check if the inputsJson is not null or empty
+if (-not [string]::IsNullOrEmpty($inputsJson)) {
+    # Parse the JSON
+    $inputs = $inputsJson | ConvertFrom-Json
 
-# Now you can access each input like:
-foreach ($key in $inputs.PSObject.Properties.Name) {
-    $envVarName = "o_inputs_$($key)"
-    $value = $inputs.$key
-    echo "$envVarName=$value"
+    # Now you can access each input like:
+    foreach ($key in $inputs.PSObject.Properties.Name) {
+        $envVarName = "o_inputs_$($key)"
+        $value = $inputs.$key
+        echo "$envVarName=$value"
+    }
+}
+else {
+    Write-Host "No inputs found or inputs JSON is null."
 }
