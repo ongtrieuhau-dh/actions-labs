e9013dc137ecd4b687f048fa03b9d94526839fbd
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-09-06 11:19:21 +0700

    CODING

--- scripts-yml/parse-env.ps1
+++ scripts-yml/parse-env.ps1
@@ -1,9 +1,28 @@
 # parse-env.ps1
 
+# Check if the secretsJson is not null or empty
+$secretsJson = $env:toJSON_secrets
+if (-not [string]::IsNullOrEmpty($secretsJson)) {
+    # Parse the JSON
+    $inputs = $secretsJson | ConvertFrom-Json
+
+    # Now you can access each input like:
+    foreach ($key in $inputs.PSObject.Properties.Name) {
+        $envVarName = ("o_secrets_$($key)").ToUpper()
+        $value = $inputs.$key
+        echo "$envVarName=$value"
+
+        $envVarName2 = ("o_inputs_$($key)").ToUpper()
+        $value2 = $inputs.$key
+        echo "$envVarName2=$value2"
+    }
+}
+else {
+    Write-Host "No secret found or secrets JSON is null."
+}
+
 # Get the inputs JSON from environment variable
 $inputsJson = $env:toJSON_inputs
-$secretsJson = $env:toJSON_secrets
-
 # Check if the inputsJson is not null or empty
 if (-not [string]::IsNullOrEmpty($inputsJson)) {
     # Parse the JSON
@@ -11,7 +30,7 @@ if (-not [string]::IsNullOrEmpty($inputsJson)) {
 
     # Now you can access each input like:
     foreach ($key in $inputs.PSObject.Properties.Name) {
-        $envVarName = "o_inputs_$($key)"
+        $envVarName = ("o_inputs_$($key)").ToUpper()
         $value = $inputs.$key
         echo "$envVarName=$value"
     }
@@ -20,18 +39,4 @@ else {
     Write-Host "No inputs found or inputs JSON is null."
 }
 
-# Check if the secretsJson is not null or empty
-if (-not [string]::IsNullOrEmpty($secretsJson)) {
-    # Parse the JSON
-    $inputs = $secretsJson | ConvertFrom-Json
 
-    # Now you can access each input like:
-    foreach ($key in $inputs.PSObject.Properties.Name) {
-        $envVarName = "o_secrets_$($key)"
-        $value = $inputs.$key
-        echo "$envVarName=$value"
-    }
-}
-else {
-    Write-Host "No secret found or secrets JSON is null."
-}
